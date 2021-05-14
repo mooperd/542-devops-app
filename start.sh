994aef05097f6517429f9367f004d9834552561f
Andrew Holway <andrew.holway@gmail.com>
2021-05-14 16:27:25 +0200

    install mysql

---
+++ start.sh
@@ -0,0 +1,8 @@
+QUERY="CREATE DATABASE $MYSQL_SCHEMA;"
+
+# Create Database
+mysql ---host $MYSQL_HOST --user $MYSQL_USER -p $MYSQL_PWD -e "$QUERY"
+
+# Run application
+gunicorn -b 0.0.0.0:8080 --access-logfile - wsgi:app
+
