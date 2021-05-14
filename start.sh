a2ee648b2f23efd1892aafffcd55f95e53486f54
Andrew Holway <andrew.holway@gmail.com>
2021-05-14 16:38:49 +0200

    mysql -error

--- start.sh
+++ start.sh
@@ -1,7 +1,9 @@
 QUERY="CREATE DATABASE $MYSQL_SCHEMA;"
 
+env
+
 # Create Database
-mysql --host $MYSQL_HOST --user $MYSQL_USER -p $MYSQL_PWD -e "$QUERY"
+mysql --host=$MYSQL_HOST --user=$MYSQL_USER --password=$MYSQL_PWD -e "$QUERY"
 
 # Run application
 gunicorn -b 0.0.0.0:8080 --access-logfile - wsgi:app
